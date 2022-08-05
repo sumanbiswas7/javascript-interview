d23b102555a8562f7744f941aeb17e11f991d63f
suman <ronaldosuman97@gmail.com>
2022-08-05 21:56:51 +0530

    add: [core-concepts] if-user-exists

---
+++ core-concepts/check if user exists/if-user-exists.js
@@ -0,0 +1,29 @@
+//? check if user with the given name exists in this users array
+
+const users = [
+  { name: "paul", age: 22 },
+  { name: "jerry", age: 18 },
+  { name: "rita", age: 25 },
+  { name: "max", age: 21 },
+  { name: "harry", age: 21 },
+];
+
+// approach - 1
+// use filter method
+
+function isUserExists(users, name) {
+  const user = users.filter((user) => user.name === name);
+  if (user.length > 0) return true;
+  return false;
+}
+
+console.log(isUserExists(users, "ma2x"));
+
+// optimal approach
+// use array.some
+const nameInput = "harry2";
+const isExist = users.some((user) => user.name === nameInput);
+
+console.log(isExist);
+
+
