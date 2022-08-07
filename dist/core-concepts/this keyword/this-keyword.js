acb686928c03ce4e24140eae494157cfd12cd5b5
suman <ronaldosuman97@gmail.com>
2022-08-07 22:19:18 +0530

    feat: swtiched to typescript

---
+++ dist/core-concepts/this keyword/this-keyword.js
@@ -0,0 +1,6 @@
+"use strict";
+//? Explaine this in javascript
+// https://www.youtube.com/watch?v=gvicrj31JOM
+// https://dmitripavlutin.com/gentle-explanation-of-this-in-javascript/
+// this -> the object that is executing the current func / the execution context
+// window in browser, globalThis in node
